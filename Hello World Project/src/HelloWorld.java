b02ee9c35980128e64571e48b4b84f3c61259522
APCS2nd <APCS2nd@MHS-LT9-15>
2019-08-21 12:17:32 -0700

    Complete Hello World project

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,12 @@
+// Ethan Robson 
+// August 16, 2019
+// First class (iconic) in Java for APCS
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
+
+}
